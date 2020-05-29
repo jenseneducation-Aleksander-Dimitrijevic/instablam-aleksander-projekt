d6020258eb8f41d506d47d842293bf8e10b6f412
Aleksander Dimitrijevic <aleksander88.dev@gmail.com>
2020-05-29 14:21:15 +0200

    init commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+  <footer>
+    <button v-if="isActive" class="bell" @click="toggleNotification" title="Blockera aviseringar">
+      <i class="far fa-bell"></i>
+    </button>
+    <button class="bell" v-else title="Tillåt aviseringar" @click="toggleNotification">
+      <i class="far fa-bell-slash"></i>
+    </button>
+    <p>Created by me, Aleksander Dimitrijevic</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footerComponent",
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    toggleNotification() {
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 2rem 0;
+  margin-top: 2rem;
+  text-align: center;
+  border-top: 1px solid #eee;
+
+  .bell {
+    border: none;
+    outline: none;
+    font-size: 2rem;
+    cursor: pointer;
+    background: none;
+    transition: 0.3s;
+    margin-bottom: 1rem;
+  }
+}
+
+@media screen and (min-width: 1440px) {
+  footer > #bell {
+    &:hover {
+      transform: scale(1.5);
+    }
+  }
+}
+</style>
